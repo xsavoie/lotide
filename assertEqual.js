23a87d6c834c3510896cf8c451377599396531a3
Xavier Savoie <xaviersavoie@gmail.com>
2021-09-13 19:55:55 +0000

    Implemented template literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,14 +1,8 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("💲️💲️💲️ Assertion Passed: " + actual + " === " + expected);
+    console.log(`💲️💲️💲️ Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log("🛑️🛑️🛑️ Assertion Failed: " + actual + " !== " + expected);
+    console.log(`🛑️🛑️🛑️ Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("xavier", "Xavier");
-assertEqual("xavier", "xavier");
-assertEqual("xavier", 1);
-assertEqual(1, 2);
