3414a859bef755208789d62941912ba7db7a255b
Xavier Savoie <xaviersavoie@gmail.com>
2021-09-14 22:51:25 +0000

    Linted eqarrays

--- eqArrays.js
+++ eqArrays.js
@@ -14,12 +14,3 @@ const eqArrays = (arrOne, arrTwo) => {
   }
 }  
 
-assertEqual(eqArrays(eqArrays([1, 2, 3], [1, 2, 3])), true);
-assertEqual(eqArrays(eqArrays([1, 2, 3], [3, 2, 1])), false);
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), false);
-
-eqArrays([1, 2, 3], [1, 2, 3]) // => true
-eqArrays([1, 2, 3], [3, 2, 1]) // => false
-
-eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
