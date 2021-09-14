fab0be6487425ca1caa0505fe3ce20ee3a349262
Xavier Savoie <xaviersavoie@gmail.com>
2021-09-14 22:36:58 +0000

    linted code

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,6 +1,6 @@
 const assertArraysEqual = (arrOne, arrTwo) => {
-  if (arrOne.length !== arrTwo.length){ 
-    console.log(`🛑️🛑️🛑️ Assertion Failed: ${arrOne} !== ${arrTwo}`)
+  if (arrOne.length !== arrTwo.length) {
+    console.log(`🛑️🛑️🛑️ Assertion Failed: ${arrOne} !== ${arrTwo}`);
     return;
   }
   for (let i = 0; i < arrOne.length; i++) {
@@ -8,11 +8,11 @@ const assertArraysEqual = (arrOne, arrTwo) => {
       console.log(`🛑️🛑️🛑️ Assertion Failed: ${arrOne} !== ${arrTwo}`);
       return;
     }
-  } 
-  console.log(`💲️💲️💲️ Assertion Passed: ${arrOne} === ${arrTwo}`)
-}
+  }
+  console.log(`💲️💲️💲️ Assertion Passed: ${arrOne} === ${arrTwo}`);
+};
 
 assertArraysEqual([1,2,3],  [1,2,3]);
 assertArraysEqual([1,2,3], [1,2,4]);
-assertArraysEqual([1, 2, 3], [1, 3, 3])
-assertArraysEqual([1,2,3], [1,2,3,4])
+assertArraysEqual([1, 2, 3], [1, 3, 3]);
+assertArraysEqual([1,2,3], [1,2,3,4]);
