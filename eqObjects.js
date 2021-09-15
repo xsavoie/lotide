a1659a6cd192d322afa451a5dd7a6c7ed8b96dac
Xavier Savoie <xaviersavoie@gmail.com>
2021-09-15 22:15:08 +0000

    linted

--- eqObjects.js
+++ eqObjects.js
@@ -15,7 +15,7 @@ const eqArrays = (arrOne, arrTwo) => {
       return true;
     }
   }
-};  
+};
 
 const eqObjects = function(objectOne, objectTwo) {
   const keyOne = Object.keys(objectOne);
@@ -28,12 +28,11 @@ const eqObjects = function(objectOne, objectTwo) {
       if (eqArrays(objectOne[key], objectTwo[key]) === false) {
         return false;
       }
-    }
-    else if (objectOne[key] !== objectTwo[key]) {
+    } else if (objectOne[key] !== objectTwo[key]) {
       return false;
     }
   } return true;
-}
+};
 
 const cd = { c: "1", d: ["2", 3] };
 const dc = { d: ["2", 3], c: "1" };
@@ -46,7 +45,7 @@ assertEqual(eqObjects(cd, cd2), false); // => false
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
 
-assertEqual(eqObjects(ab, ba), true)
+assertEqual(eqObjects(ab, ba), true);
 
 const abc = { a: "1", b: "2", c: "3" };
 assertEqual(eqObjects(ab, abc), false); // => false
