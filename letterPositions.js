2f214090307e0139d619014f19f45d41adff8612
Xavier Savoie <xaviersavoie@gmail.com>
2021-09-16 00:16:11 +0000

    Updated code to skip over spaces instead of replacing them with ''

--- letterPositions.js
+++ letterPositions.js
@@ -14,13 +14,15 @@ const assertArraysEqual = (arrOne, arrTwo) => {
 
 const letterPositions = function(sentence) {
   const results = {};
-  let lowerSentence = sentence.toLowerCase().replace(/\s/g, "");
+  let lowerSentence = sentence.toLowerCase() //.replace(/\s/g, "");
   for (let i = 0; i < lowerSentence.length; i++) {
+    if (lowerSentence[i] !== " ") {
     if (!results[lowerSentence[i]]) {
       results[lowerSentence[i]] = [];
     }
     results[lowerSentence[i]].push(i);
   }
+}
   return results;
 };
 
