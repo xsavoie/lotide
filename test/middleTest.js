73daf4b89bc30ba71c29e14c9c9edc99366da823
Xavier Savoie <xaviersavoie@gmail.com>
2021-09-20 18:13:10 +0000

    Refactored middleTest and tailtest to use mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,23 @@
-const assertArraysEqual = require("../assertArraysEqual");
+const assert = require("chai").assert;
 const middle = require("../middle");
 
-assertArraysEqual((middle([1, 2, 3])), [2]);
-assertArraysEqual((middle([1, 2, 3, 4, 5])), [3]);
-assertArraysEqual((middle([1, 2])), []);
-assertArraysEqual((middle([1, 2, 3, 4])), [2, 3]);
-assertArraysEqual((middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])), [5, 6]);
-assertArraysEqual((middle(['a', 'b', 1, 2, 3])), [1]);
+describe("#middle", () => {
+  it("returns [2] for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1,2,3]), [2]);
+  });
+  it("returns [3] for [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+  it("returns [] for [1, 2]", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+  it("returns [2, 3] for [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+  it("returns [5, 6] for [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), [5, 6]);
+  });
+  it("returns [1] for ['a', 'b', 1, 2, 3]", () => {
+    assert.deepEqual(middle(['a', 'b', 1, 2, 3]), [1]);
+  });
+});
--- test/tailtest.js
+++ test/tailtest.js
@@ -1,13 +1,29 @@
-const assertEqual = require("../assertEqual");
-const tail = require("../tail")
+const assert = require("chai").assert;
+const tail = require("../tail");
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+describe("#tail", () =>{
+  it("returns [] for [1]", () => {
+    assert.deepEqual(tail([1]), []);
+  });
+  it("returns [2,3] for [1,2,3]", () => {
+    assert.deepEqual(tail([1,2,3]), [2,3]);
+  });
+  it("returns [] for []", () => {
+    assert.deepEqual(tail([]), []);
+  });
+  it("returns ['cheese', 'pizza'] for ['large', 'cheese', 'pizza']", () => {
+    assert.deepEqual(tail(["large", "cheese", "pizza"]), ["cheese", "pizza"]);
+  });
+});
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
 
-assertEqual(tail([]));
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words); // no need to capture the return value since we are not checking it
+// assertEqual(words.length, 3); // original array should still have 3 elements!
+
+// const result = tail(["Hello", "Lighthouse", "Labs"]);
+// assertEqual(result.length, 2); // ensure we get back two elements
+// assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+// assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+
+// assertEqual(tail([]));
